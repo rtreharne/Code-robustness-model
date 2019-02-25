5fbb236b29b173cfab6294c75c49c6582a4042ca
rob.treharne <rob.treharne@gmail.com>
2019-02-25 16:49:03 +0000

    function pairwise_swaps() now returns list of codes with single pair-wise swaps

--- optimise/optimise.py
+++ optimise/optimise.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from collections import OrderedDict
-from itertools import islice
+from itertools import islice, combinations, chain
 import copy
 
 def get_ordered_gc(fname):
@@ -18,8 +18,10 @@ class CodeOptimise:
     def __init__(self, GC):
         self.GC = GC
         self.blocks = self.block_code()
-        print(self.blocks)
-        self.pairwise_swaps()
+
+        self.pairwise = self.pairwise_swaps()
+        print(len(self.pairwise))
+        print(self.pairwise[1])
 
     def block_code(self, block_size=4):
         blocks = [OrderedDict(islice(self.GC.items(), i*block_size, i*block_size+block_size)) for i in range(0, int(len(self.GC)/block_size))]
@@ -27,12 +29,18 @@ class CodeOptimise:
 
     def pairwise_swaps(self):
 
-        print(self.swap(0, 1))
-        print(self.swap(0, 2))
-        print(self.swap(0,3))
+        pairs = list(combinations(range(len(self.blocks)),2))
+        pairwise_swaps = []
 
+        for pair in pairs:
+            # does either of the pairs contain stp codon? If so ignore swap.
+            merged = list(chain(self.blocks[pair[0]].values(), self.blocks[pair[1]].values()))
+            if any('Stp' in string for string in merged):
+                print('ignore swap({0}, {1})'.format(pair[0], pair[1]), merged)
+            else:
+                pairwise_swaps.append(self.swap(pair[0], pair[1]))
 
-
+        return pairwise_swaps
 
     def swap(self, index_1, index_2):
         od_1_vals = list(self.blocks[index_1].values())
